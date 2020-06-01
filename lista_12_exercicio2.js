96a1bea398847698fe6efb500a7e57232f8e79c1
bgdj17 <bgdj17@gmail.com>
2020-06-01 14:35:54 -0300

    Exercícios lista 10. Exercício 1 lista 12

--- lista_12_exercicio2.js
+++ lista_12_exercicio2.js
@@ -1,20 +1,43 @@
-
+// Crie um algoritmo que cadastre 3 alunos e diga qual possui a maior idade, se mais de
+// um possuir maior idade apresentar o nome de todos os que possuem maior idade.
+// Para isso crie uma classe Aluno que receba duas variáveis no construtor nome e idade
+// e inicialize essas variáveis nas propriedades do objeto.
+// Após isso peça para o usuário cadastrar o primeiro aluno e solicite nome e idade.
+// Crie o objeto do aluno e adicione em um array.
+// Repita esse passo para os outros alunos.
+// Ao final o algoritmo deve iterar pelo array de alunos e descobrir quais são os alunos
+// com maior idade e imprimir seus nomes.
 var rs = require("readline-sync");
 class Alunos{
     constructor(nome, idade){
-        this.nome =nome
+        this.nome = nome
         this.idade = idade
      }
-      getIdades(){
-         return this.idade
-     }
+      getNome(){
+          return this.nome
+      }
+      getIdade(){
+          return this.idade
+      }
 }
 var cadAlunos = []
-
-for(var i =0; i<3; i++){
+var maior = 0
+var alunoMaior = 0
+var maiores =[]
+for(var i =0; i<5; i++){
     var nome= rs.question("Digite o nome. ")
-    var idade= rs.question("Digite a idade. ")
+    var idade= rs.questionInt("Digite a idade. ")
     cadAlunos.push(new Alunos(nome, idade))
+    if(cadAlunos[i].idade > maior)  {
+        maior = cadAlunos[i].idade
+        alunoMaior= cadAlunos.indexOf(cadAlunos[i])
+       
+    }
 }
-var msn= cadAlunos.getIdades
-console.log(msn)
+for(var i=0; i< cadAlunos.length; i++){
+    if(maior <=cadAlunos[i].idade){
+        maiores.push(cadAlunos[i])
+    }
+}
+
+console.log("Os alunos mais velhos são: ", maiores)
